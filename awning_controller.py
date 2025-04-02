3dcea64c494290e18d43bfd9bd442e812baafd8c
koeyismyusername <koey2182@gmail.com>
2025-04-02 09:08:58 +0900

    수동 어닝 시스템 컨트롤러 LED 제어 구현

---
+++ awning_controller.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from ncf_subscriber import NcfSubscriber
+from consts import WEB_SOCKET_PATHS, DESTINATIONS
+
+led_pin = 4
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+
+def _on_open(subs):
+    subs.subscribe()
+    
+def _on_message(subscriber, frame):
+    print(frame.body)
+    if frame.body == 'awning-on':
+        GPIO.output(led_pin,1)
+    elif frame.body == 'awning-off':
+        GPIO.output(led_pin,0)
+        
+
+subscriber = NcfSubscriber(WEB_SOCKET_PATHS['eun'], DESTINATIONS['awning'])
+subscriber.on_open = _on_open
+subscriber.on_message = _on_message
+subscriber.run_forever()
+
+GPIO.cleanup()
