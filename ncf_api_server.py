2b36642e334dd4fc58492e95c4793fc4d8a2439c
koeyismyusername <koey2182@gmail.com>
2025-04-07 14:25:08 +0900

    사용하지 않는 api 제거

--- ncf_api_server.py
+++ ncf_api_server.py
@@ -7,39 +7,7 @@ TEST_API_SERVER_PATH = "http://192.168.30.128:8081"
 
 class NcfApiServer:
     def __init__(self):
-        None
-        
-    def login(self, id, roleNames, roleFlags):
-        r = requests.post(f'{API_SERVER_PATH}/examples/login', data={'id': 2, 'roleNames': 'normal', 'roleFlags': 11})
-        return r.text
-
-    def insertTemperature(self, temperature):
-        payload = {
-            "temperature": temperature,
-            "measured-at": datetime.now().astimezone(pytz.timezone("Asia/Seoul")).isoformat()
-        }
-        r = requests.post(f"{API_SERVER_PATH}/temperatures", data=payload)
-        
-    def insertLdrValue(self, ldr_value):
-        payload = {
-            "ldr-value": ldr_value,
-            "measured-at": datetime.now().astimezone(pytz.timezone("Asia/Seoul")).isoformat()
-        }
-        requests.post(f"{API_SERVER_PATH}/ldr-values", data=payload)
-
-    def insertSoilMoistureValue(self, value):
-        payload = {
-            "soil-moisture-value": value,
-            "measured-at": datetime.now().astimezone(pytz.timezone("Asia/Seoul")).isoformat()
-        }
-        requests.post(f"{API_SERVER_PATH}/soil-moisture-values", data=payload)
-    
-    def insertHumidity(self, humidity_percentage):
-        payload = {
-            "humidity-percentage": humidity_percentage,
-            "measured-at": datetime.now().astimezone(pytz.timezone("Asia/Seoul")).isoformat()
-        }
-        requests.post(f"{API_SERVER_PATH}/humidities", data=payload)
+        pass
 
     def insert_air_temperature(self, temperature_c, position):
         payload = position
